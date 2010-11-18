81012135591c33c4d559a103acc98541ea7ce087
Josh Crews <crews.josh@gmail.com>
2010-11-17 22:15:16 -0600

    3 commit items
    skip authenticity token
    render nothing after receiving email
    remove js, css from application layout

--- app/controllers/incoming_emails_controller.rb
+++ app/controllers/incoming_emails_controller.rb
@@ -1,7 +1,8 @@
 class IncomingEmailsController < ApplicationController
+  skip_before_filter :verify_authenticity_token
   
   def create
     Friend.create(:email_address => params[:from])
-    redirect_to "/"
+    render :nothing => true
   end
 end
